1b9fbbbf7170e6508b7efc27b1866ff6ff8f207c
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-03-04 21:45:13 +0200

    Дефиниция на клас стек с последователно представяне

---
+++ lectures/stack/stack.hpp
@@ -0,0 +1,20 @@
+const int MAX_STACK = 100;
+class Stack {
+    int a[MAX_STACK];
+    int top; // индекс на последно включения елемент в стека
+public:
+    // създаване на празен стек 
+    Stack();
+
+    // проверка за празнота
+    bool empty() const;
+
+    // включване на елемент
+    void push(int x);
+
+    // изключване на елемент
+    int pop();
+
+    // поглеждане на последно включения елемент
+    int peek() const;
+}
