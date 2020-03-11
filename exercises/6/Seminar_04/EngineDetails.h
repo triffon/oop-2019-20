9c55c58101342514c6f6141c170dee8f4853be78
Yasen Alexiev <yassen4o222@abv.bg>
2020-03-11 10:18:46 +0200

    Update EngineDetails.h

--- exercises/6/Seminar_04/EngineDetails.h
+++ exercises/6/Seminar_04/EngineDetails.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-// Повече за enum class vs enum
+// РџРѕРІРµС‡Рµ Р·Р° enum class vs enum
 // https://www.geeksforgeeks.org/enum-classes-in-c-and-their-advantage-over-enum-datatype/
 
 enum class EngineType
