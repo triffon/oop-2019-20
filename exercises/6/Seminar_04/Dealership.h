d02aa43ceed41715fd3c76ab8daf77f17d4fa077
Yasen Alexiev <yassen4o222@abv.bg>
2020-03-15 10:49:04 +0200

    Update Dealership.h

--- exercises/6/Seminar_04/Dealership.h
+++ exercises/6/Seminar_04/Dealership.h
@@ -6,13 +6,24 @@ class Dealership
 {
 
 public:
-	// TODO:
 	// ТHE BIG 4
+	Dealership( size_t capacity = 8);
+	Dealership( const Dealership& other );
+	Dealership& operator=( const Dealership& other );
+	~Dealership();
+
+public:
+	void addCar( const Car& newCar );
+	void print() const;
+	void readFromConsole();
 
 private:
-	size_t size;
-	size_t capacity;
-	Car* cars;
-
+	void clean();
+	void copyFrom(const Dealership& other);
+	void resize( size_t newCapacity );
+private:
+	size_t fSize;
+	size_t fCapacity;
+	Car* fCars;
 };
 
