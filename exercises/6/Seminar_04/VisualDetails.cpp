fc6a287ccf828cf29d2b79835d4c2497a27e7106
Yasen Alexiev <yassen4o222@abv.bg>
2020-03-15 10:58:45 +0200

    Update VisualDetails.cpp

--- exercises/6/Seminar_04/VisualDetails.cpp
+++ exercises/6/Seminar_04/VisualDetails.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 
 
+VisualDetails::VisualDetails() : fDoorCount(0), fHasWing(false)
+{
+	strcpy( fColor, "" );
+}
+
 VisualDetails::VisualDetails( unsigned int doorCount, bool hasWing, const char* color) : // обикновен инициализиращ списък
 	fDoorCount(doorCount),
 	fHasWing(hasWing)
@@ -16,6 +21,25 @@ VisualDetails::VisualDetails( unsigned int doorCount, bool hasWing, const char* 
 VisualDetails::~VisualDetails()
 {
 	std::cout << "Destructor VisualDetails" << std::endl;
+}
+
+void VisualDetails::print() const
+{
+	std::cout << "Count Doors: " << fDoorCount << std::endl;
+	std::cout << ( fHasWing ? "Has wing" : "Doesn't have wing" ) << std::endl;
+	std::cout << "Color: " << fColor << std::endl;
+}
+
+void VisualDetails::readFromConsole()
+{
+	std::cout << "Enter number of doors: ";
+	std::cin >> fDoorCount;
+	std::cout << "Enter 0 if the car has no wing, otherwise - 1: ";
+	std::cin >> fHasWing;
+
+	std::cin.ignore();
+	std::cout << "Enter color: ";
+	std::cin.getline(fColor, MAX_SIZE );
 }
 
 void VisualDetails::setColor( const char* newColor )
