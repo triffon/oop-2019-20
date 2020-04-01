68208ec8040dacaa869b33fce7bc5b1c5626ae48
Alexander Dimitrov <adimitrov23@gmail.com>
2020-04-01 15:56:11 +0300

    added solutions to practice-06, group 1

--- labs/1/06-raii-and-file-streams/solutions/storing-spacecrafts/Source.cpp
+++ labs/1/06-raii-and-file-streams/solutions/storing-spacecrafts/Source.cpp
@@ -3,16 +3,43 @@
 #include "Spacecraft.h"
 #include "Vector.h"
 
-const int MAX_BUFFER_LEN = 1024;
+bool readInput(const char* filePath, Vector& container);
+bool storeSpacecrafts(const char* filePath, Vector& container);
+bool getSpacecrafts(const char* filePath, Vector& container);
 
 int main()
 {
     Vector vec;
-    std::ifstream inTxt("input.txt");
-    if (!inTxt) {
-        std::cout << "Couldn't open file for reading!" << std::endl;
+    if (!readInput("input.txt", vec)) {
+        std::cout << "Couldn't open file input.txt for reading!" << std::endl;
         return -1;
     }
+
+    if (!storeSpacecrafts("database.bin", vec)) {
+        std::cout << "Couldn't open file database.bin for writing to store spacecrafts!" << std::endl;
+        return -1;
+    }
+
+    Vector vecNew;
+    if (!getSpacecrafts("database.bin", vecNew)) {
+        std::cout << "Couldn't open file database.bin for reading to get spacecrafts!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Original vector:" << std::endl << vec << std::endl << std::endl;
+    std::cout << "Vector from the database:" << std::endl << vecNew << std::endl;
+
+    return 0;
+}
+
+
+bool readInput(const char* filePath, Vector& container)
+{
+    const int MAX_BUFFER_LEN = 1024;
+
+    std::ifstream inTxt(filePath);
+    if (!inTxt)
+        return false;
 
     // Reading the whole file
     while (!inTxt.eof()) {
@@ -21,15 +48,35 @@ int main()
         inTxt >> year;
         inTxt.ignore();
         inTxt.getline(name, MAX_BUFFER_LEN);
-        vec.push_back({year, name});
+        container.push_back({year, name});
     }
 
     inTxt.close();
+    return true;
+}
 
-    // Homework:
-    // 1) Add operator<< to Spacecraft
-    // 2) Add serialize method and constructor(std::ifstream&) to Vector
-    // 3) Finish the solution
 
-    return 0;
+bool storeSpacecrafts(const char* filePath, Vector& container)
+{
+    std::ofstream out(filePath, std::ios::binary);
+    if (!out)
+        return false;
+
+    container.serialize(out);
+
+    out.close();
+    return true;
+}
+
+
+bool getSpacecrafts(const char* filePath, Vector& container)
+{
+    std::ifstream in(filePath, std::ios::binary);
+    if (!in)
+        return false;
+
+    container.deserialize(in);
+
+    in.close();
+    return true;
 }
