d6ee875deb9a0575fa92aaea788095f150a54e0c
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-04-08 10:04:43 +0300

    Примери за работа с файлове

--- lectures/files/main.cpp
+++ lectures/files/main.cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+
+void printTextFile(char const* filename) {
+    std::ifstream fi(filename);
+    const int MAX = 1000;
+    char line[MAX] = "";
+    unsigned counter = 0;
+    while (fi.getline(line, MAX))
+        std::cout << ++counter << ": " << line << std::endl;
+}
+
+void printBinaryFile(char const* filename) {
+    std::ifstream fi(filename, std::ios::in | std::ios::binary);
+    const int MAX = 1000;
+    char byte = 0; // [-128; 127]
+    // unsigned char: [0; 255]
+    unsigned counter = 0;
+    while (fi.get(byte))
+        std::cout << std::dec << ++counter << ": " << std::hex << (int)(unsigned char)byte << std::endl;
+}
+
+void writeBottlesSong(char const* filename, int bottles = 10) {
+    std::ofstream fo(filename);
+    for(; bottles > 0; bottles--)
+        fo << "There are " << bottles << " green bottles hanging on the wall,\n"
+           << "there are " << bottles << " green bottles hanging on the wall,\n"
+           << "and if one green bottle accidentally falls\n"
+           << "there will be " << bottles - 1 << " green bottles hanging on the wall!\n\n";
+}
+
+void countInBinary(char const *filename, int to = 10, int from = 0) {
+    std::ofstream fo(filename, std::ios::out | std::ios::binary);
+    for(char i = from; i <= to; i++)
+        fo.put(i);
+}
 
 bool safeReadInt(int& x, std::istream& is = std::cin) {
     std::cout << "Моля, въведете цяло число: ";
@@ -38,6 +74,13 @@ void testSafeRead() {
 
 int main() {
 //    testistream();
-    testSafeRead();
+    // testSafeRead();
+    // printTextFile("main.cpp");
+    // printBinaryFile("build/files");
+    // writeBottlesSong("bottles10.txt");
+    // writeBottlesSong("bottles100.txt", 100);
+    countInBinary("numbers.bin", 100);
+    countInBinary("numbers.bin", 50, 30);
+    printBinaryFile("numbers.bin");
     return 0;
 }
