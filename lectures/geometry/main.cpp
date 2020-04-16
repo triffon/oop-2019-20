385d7b43ae1f07954366615cee421c380639dadf
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-04-16 11:42:39 +0300

    Шаблонизиране на класа Point2D

--- lectures/geometry/main.cpp
+++ lectures/geometry/main.cpp
@@ -2,6 +2,9 @@
 #include "point2d.hpp"
 #include "point3d.hpp"
 
+using DPoint2D = Point2D<double>;
+// typedef Point2D<double> DPoint2D;
+
 void testPoint3D() {
     Point3D p;
     Point3D q(1, 3, 8);
@@ -15,14 +18,16 @@ void testPoint3D() {
 
 void testPoint2D() {
     // std::cout << "Hello, world!\n";
-    Point2D p;
-    Point2D q(1, 2);
+    DPoint2D p;
+    DPoint2D q(1, 2);
     p.printnl();
     q.printnl();
     q.translate(q);
     q.translate(1, 0);
     q.printnl();
     std::cout << p.distanceTo(q) << std::endl;
+    Point2D<int> r;
+ // !!!   std::cout << p.distanceTo(r) << std::endl;
 }
 
 int main() {
--- lectures/geometry/point3d.hpp
+++ lectures/geometry/point3d.hpp
@@ -3,31 +3,33 @@
 
 #include "point2d.hpp"
 
+using DPoint2D = Point2D<double>;
+
 class Point3D {
 // double x, y, z;
 // ниво 0
-    Point2D p;
+    DPoint2D p;
     double z;
 public:
 // ниво 1
     Point3D() {
         // !!! p.x = p.y = z = 0;
-        p = Point2D();
+        p = DPoint2D();
         z = 0;
     }
     Point3D(double _x, double _y, double _z) {
-        p = Point2D(_x, _y);
+        p = DPoint2D(_x, _y);
         z = _z;
     }
-    Point3D(Point2D const& _p, double _z) { p = _p; z = _z; }
+    Point3D(DPoint2D const& _p, double _z) { p = _p; z = _z; }
 
     // селектори
-    Point2D getP() const { return p; }
+    DPoint2D getP() const { return p; }
     double getX() const { return p.getX(); }
     double getY() const { return p.getY(); }
     double getZ() const { return z; }
 
-    void setP(Point2D const& _p) { p =  _p; }
+    void setP(DPoint2D const& _p) { p =  _p; }
     void setX(double _x) { p.setX(_x); }
     void setY(double _y) { p.setY(_y); }
     void setZ(double _z) { z = _z; }
