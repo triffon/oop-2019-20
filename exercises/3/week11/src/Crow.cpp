682da441e7ba17f9a4669b262f0d357f91aecb58
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-04-30 17:23:17 +0300

    week 12 skeleton

--- exercises/3/week11/src/Crow.cpp
+++
@@ -1,6 +0,0 @@
-#include"include/Crow.h"
-#include<iostream>
-using namespace std;
-void Crow::fly(){
-	cout << "Caw! I am a crow!" << endl;
-};
--- exercises/3/week11/src/include/Bird.h
+++
@@ -1,9 +0,0 @@
-#ifndef BIRD_H
-#define BIRD_H
-
-class Bird{
-public:
-	virtual void fly()=0;
-};
-
-#endif
