939ec0f0d6263257b18082e5c080164fbf11a95b
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-05-05 06:36:07 +0300

    add build systems demo

---
+++ exercises/3/week12.2/src/Crow.cpp
@@ -0,0 +1,6 @@
+#include"Crow.h"
+#include<iostream>
+using namespace std;
+void Crow::fly(){
+	cout << "Caw! I am a crow!" << endl;
+};
---
+++ exercises/3/week12.2/src/main.cpp
@@ -0,0 +1,13 @@
+#include"Crow.h"
+#include"Penguin.h"
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	Crow c;
+	c.fly();
+	Penguin p;
+	p.fly();
+	return 0;
+}
