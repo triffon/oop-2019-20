24b787d148b5a58b5a25055359f500cbf9e5887a
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-05-27 11:20:32 +0300

    Примерни тестове за QuickTask с doctest

--- lectures/tasks/main.cpp
+++ lectures/tasks/main.cpp
@@ -1,14 +1,3 @@
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include "quicktask.hpp"
-
-void testQuickTask() {
-    QuickTask trash("изхвърляне на боклука");
-    std::cout << trash;
-    trash.work();
-    std::cout << trash;
-}
-
-int main() {
-    testQuickTask();
-}
+#include "quicktask_tests.hpp"
---
+++ lectures/tasks/tests/quicktask_tests.hpp
@@ -0,0 +1,61 @@
+#include <sstream>
+#include "doctest.h"
+#include "../quicktask.hpp"
+
+const char NAME[] = "изхвърляне на боклука";
+
+TEST_CASE("Бърза задача с име се извежда коректно") {
+    QuickTask qt(NAME);
+    std::ostringstream os;
+    
+    /*
+    std::ofstream os("test.txt");
+    qt.print(os);
+    os.close();
+
+    std::ifstream is("test.txt");
+    const int MAX = 1000;
+    char line[MAX];
+    is.getline(line, MAX);
+    is.close();
+
+    CHECK_NE ( line, "" );
+    CHECK_EQ ( line, "abc");
+    */
+
+    qt.print(os);
+    CHECK_NE ( os.str(), "");
+    CHECK_EQ ( os.str(), "бърза задача 'изхвърляне на боклука', която НЕ е завършена" );
+}
+
+TEST_CASE("Бързата задача при създаване не е завършена") {
+    QuickTask qt(NAME);
+    CHECK_EQ( qt.getProgress(), 0);
+    CHECK( !qt.isFinished() );
+}
+
+TEST_CASE("Бързата задача не се завършва при работа 0 единици време") {
+    QuickTask qt(NAME);
+    CHECK_EQ( qt.work(0), 0 );
+    CHECK_EQ( qt.getProgress(), 0);
+    CHECK( !qt.isFinished() );
+}
+
+TEST_CASE("Бързата задача се завършва при работа 1 единица време") {
+    QuickTask qt(NAME);
+    CHECK_EQ (qt.work(1), 0);
+    CHECK_EQ( qt.getProgress(), 1);
+    CHECK( qt.isFinished() );
+}
+
+TEST_CASE("Бързата задача винаги има време за изпълнение 1") {
+    QuickTask qt(NAME);
+    CHECK_EQ( qt.getExecutionTime(), 1 );
+}
+
+TEST_CASE("Бързата задача се завършва при работа повече от 1 единица време") {
+    QuickTask qt(NAME);
+    CHECK_EQ (qt.work(10), 9);
+    CHECK_EQ( qt.getProgress(), 1);
+    CHECK( qt.isFinished() );
+}
