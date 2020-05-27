1979a17142605f9ae92ab65c8ce28da22cea9a83
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2020-05-27 11:14:05 +0300

    Примери за използване на твърдения (assertions)

--- lectures/common/named.cpp
+++ lectures/common/named.cpp
@@ -1,7 +1,9 @@
+#include <cassert>
 #include <cstring>
 #include "named.hpp"
 
 void Named::setName(char const* n) {
+    assert(n != nullptr);
     // разчитаме, че за name има заделена памет и в нея има записано нещо
     // изтриваме старата памет
     delete[] name;
