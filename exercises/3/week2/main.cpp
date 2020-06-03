a28f0583f586ee9c2ce2e9f9028956312e9f992c
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-06-04 02:55:25 +0300

    add week2 documentation

--- exercises/3/week2/main.cpp
+++ exercises/3/week2/main.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+//This is forward declaration
+//It tells the compiler that such class exists and we will supply it later
+//We use it to deal with cyclic dependencies like this:
+//Author requires Book which requires Author which requires Book
 class Book;
 class Author{
     char name[10];
     Book* books;
 };
+//Here we actually initialize the class Book
 class Book{
     char name[10];
     Author* a;
--- exercises/3/week2/main3.cpp
+++ exercises/3/week2/main3.cpp
@@ -2,6 +2,7 @@
 #include<cstring>
 #include"Student.h"
 using namespace std;
+//We could create a rational number abstraction using this class
 class PositiveRationalNumber{
 int chislitel;
 int znam;
@@ -19,6 +20,19 @@ float setValue(float value){
     znam = 100;
 }
 };
+//However we could also use the following class
+class PositiveRationalNumber2{
+float value;
+public:
+void setValue(float f){
+	this->value = f;
+}
+float getValue(){
+	return value;
+}
+};
+//These two classes are realization of the same abstraction
+//They only differ internally
 int main(){
     PositiveRationalNumber rn;
     rn.setValue(-3.14);
